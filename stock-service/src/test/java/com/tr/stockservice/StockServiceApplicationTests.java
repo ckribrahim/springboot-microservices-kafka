e6d4a0d9ac7010418389d514d4185ce85888327a
Ibrahim <firtinagtrnn@gmail.com>
2024-02-14 23:31:44 +0300

    Initial commit

---
+++ stock-service/src/test/java/com/tr/stockservice/StockServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tr.stockservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
